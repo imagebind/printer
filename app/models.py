8248dc6a4aecb5c804140accb0bb39c890898e55
imagebind <kravikumars444@gmail.com>
2024-11-17 15:50:09 +0530

    fix cell number

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from datetime import timedelta
-
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
@@ -40,7 +40,10 @@ class Plan(models.Model):
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     father_name = models.CharField(max_length=100, null=True)
-    cell_number = models.IntegerField()
+    cell_number = models.CharField(
+        max_length=10, 
+        validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Enter a valid phone number.")]
+    )
     email = models.EmailField(unique=True)
     flat_no = models.CharField(max_length=10, null=True)
     flat_name = models.CharField(max_length=100, null=True)
